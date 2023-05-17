07c309eb64f19512b52b24b3e0acc01642b8c70c
Laiquah <cotielaiquah@gmail.com>
2023-05-17 10:17:21 +0200

    added chnages to node.js

---
+++ script/node.js
@@ -0,0 +1,17 @@
+let person = {
+    name: 'Laiquah',
+    surname: 'Cotie',
+    age:'19',
+}
+
+let someone = {
+    name:'Zareenah',
+    surname:'April',
+    age:'12',
+}
+
+console.log(person)
+console.log(someone)
+
+let people = ['Laiquah', 'Cotie', '19', 'Zareenah', 'April', '12']
+console.log(people)
